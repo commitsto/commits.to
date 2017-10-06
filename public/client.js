45a84ccac15c2dac679aa3df93d9e2b75a56ca73
System <none>
2017-10-06 10:18:28 +0000

    define and link routes, replace jquery dom rendering with handlebars

--- public/client.js
+++ public/client.js
@@ -1,28 +1,6 @@
 // --------------------------------- 80chars ---------------------------------->
-// replace with handlebars! [what does that mean? isn't that done?]
-
+// FIXME left over from testing
 $(function() {
-  let user = $('#promises-section').data("user")
-  $.get('/promises/'+user, function(users) {
-    console.log('users', users)
-    let $list = $('<ul/>', {class: 'user-list'})
-    
-    for (var user in users) {
-      console.log('keys', users[user]);
-      let $item = $('<li/>');
-      let $nest = $('<ul/>', {class: 'promises-list'});
-      
-      users[user].forEach(function(promise) {
-        // moment(promise.tdue).calendar() // if tdue were unixtime
-        $nest.append(
-          `<li><a href="/${promise.urtx}">${promise.what} by ${promise.tdue}</a></li>`)
-      });
-      
-      $list.append($item.append(`<a>${user}</a>`).append($nest));
-    };
-    $list.appendTo('#promises-section');
-  });
-
   $('form').submit(function(event) {
     event.preventDefault();
     var p = $('input#p').val();

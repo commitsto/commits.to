115ee023b838ea860fef64042e64386f0746fa08
Glitch (iwill) <none>
2017-11-25 17:03:38 +0000

    virtual, live-calculating credit field, refactor duplicated credit parsing logic

--- helpers/calculate.js
+++ helpers/calculate.js
@@ -1,20 +1,10 @@
-import moment from 'moment-timezone'
 import Handlebars from 'handlebars'
+import { hoursFromNow } from '../lib/parse'
 
-import computeCredit from '../lib/latepenalty'
-
-export const hoursUntil = function(dueDate) {
-  const end = moment.tz(dueDate, 'America/New_York')
-  let diff = moment().diff(end)
-  diff = moment.duration(diff).asHours()
-  
-  // console.log('hoursUntil', dueDate, diff)
-  return Math.abs(diff)
-}
 
 Handlebars.registerHelper('dueStatus', function(dueDate) {
   if (!dueDate) return
   
   // console.log('dueStatus', dueDate)
-  return hoursUntil(dueDate)
+  return hoursFromNow(dueDate)
 })

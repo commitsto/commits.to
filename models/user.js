f96971b4b62004689c4628912e58d6965cff2288
Glitch (iwill) <none>
2017-11-28 17:53:22 +0000

    🙎👴 Updated with Glitch

---
+++ models/user.js
@@ -0,0 +1,32 @@
+// --------------------------------- 80chars ---------------------------------->
+import Sequelize from 'sequelize'
+import moment from 'moment-timezone'
+
+import Promises, { sequelize } from './promise'
+
+const Users = sequelize.define('users', {
+  name: { // username
+    type: Sequelize.STRING,
+    primaryKey: true,
+    unique: true,
+  },
+})
+
+// all tables created
+// sequelize.sync().then(() => {
+  
+// })
+
+Users.hasMany(Promises, { foreignKey: 'user', sourceKey: 'name', constraints: false })
+
+// sequelize.authenticate()
+//   .then(function(err) {
+//     console.log('Database connection established')
+//   })
+//   .catch(function (err) {
+//     console.log('Database connection error: ', err)
+//   })
+
+export default Users
+
+// --------------------------------- 80chars ---------------------------------->

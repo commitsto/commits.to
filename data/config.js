4bb33478c27e7c5f461f82dd0aeeecf8526e8eba
Chris Butler <chris@platformthirteen.com>
2017-12-26 21:44:09 -0500

    add DATABASE_URL to .env

--- data/config.js
+++ data/config.js
@@ -8,6 +8,8 @@ export const APP_PATH = env.APP_PATH
 export const ENV_NAME = env.ENV_NAME
 export const PORT = env.PORT
 
+export const DATABASE_URL = env.DATABASE_URL
+
 export const MAILGUN_DOMAIN = env.MAILGUN_DOMAIN
 export const MAILGUN_KEY    = env.MAILGUN_KEY
 export const MAILGUN_FROM   = env.MAILGUN_FROM

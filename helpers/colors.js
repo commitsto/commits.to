bf51a7109a7d24ffb5637acea0b1f5c4505644a5
Chris Butler <chris@platformthirteen.com>
2017-12-30 21:41:10 -0500

    add user reliability display to promise card

--- helpers/colors.js
+++ helpers/colors.js
@@ -1,49 +1,59 @@
-import moment from 'moment-timezone'
 import Handlebars from 'handlebars'
 
-Handlebars.registerHelper('creditColor', function(credit) {
-  let color = 'green';
-  
+const colorFromPercent = (score) => {
+  let color = 'green'
+
   switch (true) {
-    case (credit == null):
-      color = '' //FIXME
-      break
-    case (credit < .5):
-      color = 'red'
-      break
-    case (credit < .9):
-      color = 'orange'
-      break
-    case (credit < 1):
-      color = 'blue'
-      break
+  case (score == null):
+    color = '' //FIXME
+    break
+  case (score < .5):
+    color = 'red'
+    break
+  case (score < .9):
+    color = 'orange'
+    break
+  case (score < 1):
+    color = 'blue'
+    break
   }
-  
+
+  return color
+}
+
+Handlebars.registerHelper('scoreColor', function(score) {
+  const color = colorFromPercent(score)
+  // console.log('scoreColor', credit, color)
+  return `reliability--status-${color}`
+})
+
+Handlebars.registerHelper('creditColor', function(credit) {
+  const color = colorFromPercent(credit)
   // console.log('creditColor', credit, color)
   return `credit--status-${color}`
-});
+})
 
 Handlebars.registerHelper('dueColor', function(diff) {
   let color = 'gray'
-  
+
   switch (true) {
-    case(diff < -(24 * 5)):
-      color = 'green'
-      break
-    case(diff < -(24 * 2)):
-      color = 'blue'
-      break
-    case(diff < -24):
-      color = 'yellow'
-      break
-      
-    case(diff < 0):
-      color = 'orange'  
-      break
-    case (diff > 0):
-      color = 'red'
+  case(diff < -(24 * 5)):
+    color = 'green'
+    break
+  case(diff < -(24 * 2)):
+    color = 'blue'
+    break
+  case(diff < -24):
+    color = 'yellow'
+    break
+
+  case(diff < 0):
+    color = 'orange'
+    break
+  case (diff > 0):
+    color = 'red'
   }
-  
+
   // console.log('dueColor', diff, color)
   return `promise--status-${color}`
-});
+})
--- helpers/format.js
+++ helpers/format.js
@@ -3,33 +3,33 @@ import Handlebars from 'handlebars'
 
 Handlebars.registerHelper('calendarUrl', function(promise) {
   if (!promise) return
-  
+
   // FIXME timezone
-  const isoDate = moment.tz(promise.tdue, 'America/New_York').format("YYYYMMDDTHHmmss")
+  const isoDate = moment.tz(promise.tdue, 'America/New_York').format('YYYYMMDDTHHmmss')
   const baseUrl = 'https://calendar.google.com/calendar/event'
   const query = `?action=TEMPLATE&text=${promise.user}%20${promise.domain}%20${promise.what}&dates=${isoDate}/${isoDate}&details=${promise.id}`
-  
+
   // console.log('isoDate', date, isoDate)
   return baseUrl + query
-});
+})
 
 Handlebars.registerHelper('prettyDate', function(date) {
   if (!date) return
   //const pDate = moment.tz(date, 'America/New_York').format('MMMM Do YYYY, h:mm:ss a')
   const pDate = moment.tz(date, 'America/New_York').format('YYYY-MM-DD HH:mm:ss ddd (UTCZZ)')
-  
+
   // console.log('prettyDate', date, pDate)
   return pDate
-});
+})
 
 Handlebars.registerHelper('prettyPercent', function(number) {
   if (!number) return
-  return `${(number * 100).toFixed(2)}%`
-});
+  return `${(number * 100).toFixed(3)}%`
+})
 
 Handlebars.registerHelper('verbifyDomain', function(opts) {
   const domain = opts.hash && opts.hash.domain
-  
+
   // console.log('verbifyDomain', domain)
   return domain && domain.replace('.to', ' to')
-});
+})

d40830731645b1315db7e6fe0aaf1cd44a49c073
Chris Butler <chris@platformthirteen.com>
2018-01-06 15:23:44 -0500

    fix promise completion API

--- app/api.js
+++ app/api.js
@@ -2,48 +2,53 @@ import app from './express'
 import moment from 'moment-timezone'
 
 import mailself from '../lib/mail'
-import {cache, seed, setup, importJson} from '../data/seed'
+import { cache, seed, setup, importJson } from '../data/seed'
 
-import Promises, {sequelize} from '../models/promise'
+import Promises, { sequelize } from '../models/promise'
+import { Users } from '../models/user'
 import parsePromise from '../lib/parse/promise'
 import parseCredit from '../lib/parse/credit'
 
+const userQuery = (user) => ({
+  model: Users,
+  where: { username: user }
+})
+
 // Actions
 
-app.get('/promises/remove/:id(*)', (req, resp) => {
+app.get('/_s/:user/promises/remove/:slug(*)', (req, resp) => {
   console.log('remove', req.params)
   // FIXME: refactor/secure this
   Promises.destroy({
     where: {
-      id: req.params.id
-    }
+      slug: req.params.slug
+    },
+    include: [userQuery(req.params.user)],
   }).then(function(deletedRows) {
     console.log('promise removed', deletedRows)
     resp.redirect('/')
   })
 })
 
-app.get('/promises/:user/remove', function(req, resp) {
-  var dbPromises = {}
+app.get('/_s/:user/promises/remove', function(req, resp) {
   Promises.destroy({
-    where: {
-      user: req.params.user
-    }
+    include: [userQuery(req.params.user)],
   }).then(function(deletedRows) {
     console.log('user promises removed', deletedRows)
     resp.redirect('/')
   })
 })
 
-app.get('/promises/complete/:id(*)', (req, resp) => {
+app.get('/_s/:user/promises/complete/:slug(*)', (req, resp) => {
   Promises.findOne({
     where: {
-      id: req.params.id
-    }
+      slug: req.params.slug
+    },
+    include: [userQuery(req.params.user)],
   }).then(function(promise) {
     promise.update({
       tfin: moment(), //.tz('America/New_York')  FIXME,
-      cred: parseCredit({dueDate: promise.tdue})
+      cred: parseCredit({ dueDate: promise.tdue })
     })
 
     console.log('complete promise', promise.dataValues)
@@ -51,16 +56,17 @@ app.get('/promises/complete/:id(*)', (req, resp) => {
   })
 })
 
-app.post('/promises/edit/:id(*)', (req, res) => {
+app.post('/_s/:user/promises/edit/:id(*)', (req, res) => {
   console.log('edit promise', req.params.id, req.body)
 
   Promises.find({
     where: {
       id: req.params.id
-    }
+    },
+    include: [userQuery(req.params.user)],
   }).then(function(promise) {
     promise.update({
-      cred: parseCredit({dueDate: promise.tdue, finishDate: promise.tfin}),
+      cred: parseCredit({ dueDate: promise.tdue, finishDate: promise.tfin }),
       ...req.body
     })
 
@@ -73,22 +79,24 @@ app.post('/promises/edit/:id(*)', (req, res) => {
   })
 })
 
-app.get('/promises/create/:urtext(*)', (req, resp) => {
-  console.log('create', req.params)
-  parsePromise({urtext: req.params.urtext, ip: req.ip}).then((parsedPromise) => {
-    Promises.create(parsedPromise).then(function(promise) {
-      console.log('promise created', promise.dataValues)
-      mailself('PROMISE', promise.urtext) // send dreeves@ an email
-      resp.redirect(`/${req.params.urtext}`)
-    })
-  })
-})
+// Deprecated because we no longer use /create - FIXME: delete?
+//
+// app.get('/promises/create/:urtext(*)', (req, resp) => {
+//   console.log('create', req.params)
+//   parsePromise({urtext: req.params.urtext, ip: req.ip}).then((parsedPromise) => {
+//     Promises.create(parsedPromise).then(function(promise) {
+//       console.log('promise created', promise.dataValues)
+//       mailself('PROMISE', promise.urtext) // send dreeves@ an email
+//       resp.redirect(`/${req.params.urtext}`)
+//     })
+//   })
+// })
 
 // Endpoints
 
 app.get('/promise/:udp/:urtext', function(req, resp) {
   let urtext = req.originalUrl.substr(9)
-  Promises.findOne({where: { urtext }}).then(function(promise) {
+  Promises.findOne({ where: { urtext } }).then(function(promise) {
     console.log('single promise', urtext, promise)
     // resp.write(promise)
     resp.json(promise)
@@ -97,8 +105,8 @@ app.get('/promise/:udp/:urtext', function(req, resp) {
 })
 
 app.get('/promises', function(req, resp) {
-  var dbPromises = {}
-  Promises.findAll({order: sequelize.literal('tdue DESC')}).then(function(promises) {
+  let dbPromises = {}
+  Promises.findAll({ order: sequelize.literal('tdue DESC') }).then(function(promises) {
     // console.log('all promises', promises)
     // create nested array of promises by user:
     promises.forEach(function(promise) {
@@ -110,7 +118,7 @@ app.get('/promises', function(req, resp) {
 })
 
 app.get('/promises/:user', function(req, resp) {
-  var dbPromises = {}
+  let dbPromises = {}
   Promises.findAll({
     where: {
       user: req.params.user
@@ -150,6 +158,6 @@ app.get('/cache', (req, resp) => {
 
 // removes all entries from the promises table
 app.get('/empty', (req, resp) => {
-  Promises.destroy({where: {}})
+  Promises.destroy({ where: {} })
   resp.redirect('/')
 })
--- public/js/promise.js
+++ public/js/promise.js
@@ -1,8 +1,8 @@
-var swal = swal || {};
+var swal = swal || {}
 
 var completePromiseText = {
   title: 'Mark this promise completed?',
-  text: "You won't be able to revert this!",
+  text: 'You won\'t be able to revert this!',
   type: 'warning',
   showCancelButton: true,
   confirmButtonColor: '#3085d6',
@@ -22,12 +22,16 @@ var completePromiseText = {
 //   cancelButtonText: 'No, cancel!'
 // }
 
-function completePromise(id) {
-  console.log('completePromise', id);
-  
-  swal(completePromiseText).then(function () {
-    fetch(`/promises/complete/${id}`).then(function(response) {
-      if(response.ok) {
+var completePromise = function(username, slug) {
+  console.log('completePromise', slug, username)
+  var subdomainTest = new RegExp(/^([a-z]+\:\/{2})?([\w-]+\.[\w-]+\.\w+)$/)
+  var isSubdomain = !!('window.location.hostname').match(subdomainTest)
+
+  var apiUrl = `${ isSubdomain ? '' : `/_s/${username}` }/promises/complete/${slug}`
+
+  swal(completePromiseText).then(function() {
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
         return swal(
           'Completed!',
           'Your promise has been fulfilled.',
@@ -36,15 +40,13 @@ function completePromise(id) {
       }
       throw new Error('Network response was not ok.')
     })
-  }, function (dismiss) {
+  }, function(dismiss) {
     // dismiss can be 'cancel', 'overlay',
     // 'close', and 'timer'
-    if (dismiss === 'cancel') {
-
-    }
+    // if (dismiss === 'cancel') {}
   })
 }
 
-function editPromise(id) {
+var editPromise = function(id) {
   fetch('/promises/edit/${id}')
 }

887bdd065cb5c559466757a8365e693ede19123b
Daniel Reeves <dreeves@gmail.com>
2018-01-08 12:53:58 -0800

    Add user mike

--- data/seed.js
+++ data/seed.js
@@ -104,7 +104,8 @@ export const users = [
   /* contributors */
   'chris', 'stephen', 'temujin9',
   /* invitees */
-  'pierre', 'chelsea', 'forrest'
+  'pierre', 'chelsea', 'forrest', 
+  'mike',
 ]
 
 // dreev calls dibs on 'danny', 'dan', & 'd' in case we implement aliases

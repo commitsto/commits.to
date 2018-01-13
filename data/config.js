9788fa006688aaf71508d446ab39eff5ee2918b3
Chris Butler <chris@platformthirteen.com>
2018-01-13 11:23:29 -0500

    tweak env vars and footer

--- data/config.js
+++ data/config.js
@@ -1,8 +1,8 @@
-if (process.env.ENVIRONMENT !== 'production') {
+const env = process.env
+
+if (env.ENVIRONMENT !== 'production' && env.ENVIRONMENT !== 'staging') {
   require('dotenv').config()
 }
-
-const env = process.env
 
 export const APP_DOMAIN = env.APP_DOMAIN
 export const APP_PATH = env.APP_PATH
@@ -13,9 +13,8 @@ export const ALLOW_ADMIN_ACTIONS = env.ALLOW_ADMIN_ACTIONS
 export const DATABASE_URL = env.DATABASE_URL
 
 export const MAILGUN_DOMAIN = env.MAILGUN_DOMAIN
-export const MAILGUN_KEY    = env.MAILGUN_KEY
-export const MAILGUN_FROM   = env.MAILGUN_FROM
-export const MAILGUN_TO     = env.MAILGUN_TO
+export const MAILGUN_KEY = env.MAILGUN_KEY
+export const MAILGUN_FROM = env.MAILGUN_FROM
+export const MAILGUN_TO = env.MAILGUN_TO
 
-// default domain
-export default (APP_DOMAIN || 'commits.to')
+export default (APP_DOMAIN || 'commits.to') // default domain

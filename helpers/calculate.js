8355b2ec0a5e37173c840fe0af4e1252faface06
Chris Butler <chris@platformthirteen.com>
2018-02-21 21:06:24 -0500

    add helper for card classes

--- helpers/calculate.js
+++ helpers/calculate.js
@@ -1,10 +1,23 @@
 import Handlebars from 'handlebars'
+import _ from 'lodash'
+
 import { timeDiff } from '../lib/parse/time'
 
-
-Handlebars.registerHelper('dueStatus', function(dueDate) {
-  if (!dueDate) return
+Handlebars.registerHelper('dueStatus', (dueDate) => {
+  if (!dueDate) return ''
 
   // console.log('dueStatus', dueDate)
   return timeDiff({ dueDate, units: 'hours' })
+})
+
+Handlebars.registerHelper('cardClassesFor', (promise) => {
+  if (!promise) return {}
+
+  const classes = _({ completed: promise.tfin, voided: promise.void })
+    .pickBy()
+    .keys()
+    .join(' ')
+
+  console.log('cardClassesFor', promise.id, classes)
+  return classes
 })

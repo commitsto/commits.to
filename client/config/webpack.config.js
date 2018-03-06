80b75a367edb71507bd5545d8be23e5fface514c
Chris Butler <chris@platformthirteen.com>
2018-03-05 19:57:39 -0500

    configure webpack and babel-loader, add eslint

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -1,3 +1,20 @@
+const path = require('path')
+
 module.exports = {
-  
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../build'),
+  },
 }
---
+++ client/src/index.js
@@ -0,0 +1,3 @@
+const logger = (msg) => console.log(msg)
+
+logger('entrypoint!')

bf40429a90343864a78aa525c2456294a577bc34
Chris Butler <chris@platformthirteen.com>
2018-05-19 14:58:24 -0400

    refactor url parsing test

--- test/lib/parse/url.js
+++ test/lib/parse/url.js
@@ -4,23 +4,24 @@ import isValidUrl from '../../../lib/parse/url'
 
 describe('isValidUrl', () => {
   def('url', () => '/go_for_a_run/by/2:30pm')
+  def('isValid', () => isValidUrl({ url: $url }))
 
   it('allows any url that has no invalid characters', () => {
-    expect(isValidUrl({ url: $url })).to.be.true
+    expect($isValid).to.be.true
   })
 
   context('when the url has a file extension', () => {
     def('url', () => '/awekjad.txt')
 
     it('rejects the url', () => {
-      expect(isValidUrl({ url: $url })).to.be.false
+      expect($isValid).to.be.false
     })
 
     context('when the url ends in /by', () => {
       def('url', () => '/test/this/thing/by')
 
       it('rejects the url', () => {
-        expect(isValidUrl({ url: $url })).to.be.false
+        expect($isValid).to.be.false
       })
     })
 
@@ -28,7 +29,7 @@ describe('isValidUrl', () => {
       def('url', () => '/tester.commits.to/test/the/things')
 
       it('rejects the url', () => {
-        expect(isValidUrl({ url: $url })).to.be.false
+        expect($isValid).to.be.false
       })
     })
   })

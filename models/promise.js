780d3bae432549f2846fe9eca17b1ad14789966f
Daniel Reeves <dreeves@beeminder.com>
2018-06-27 22:34:40 -0700

    Sort by urgency based on Schelling fence proximity

--- models/promise.js
+++ models/promise.js
@@ -50,18 +50,50 @@ export default sequelize.define('promises', { // sequelize needs the doublequote
   ]
 })
 
-export const promiseGallerySort = (a, b) => {
-  // pending promises are sorted by due date (tdue) ascending
-  // completed promises are sorted by completion date (tfin) descending
-  // completed promises sort after pending promises
+const SID = 86400      // seconds in a day   ( NB: treat 30d as 1mo & 365d as )
+const SIW = 7   * SID  // seconds in a week  ( 1yr so that Schelling fence    )
+const SIM = 30  * SID  // seconds in a month ( deadlines are the same time of )
+const SIY = 365 * SID  // seconds in a year  ( day as the original deadline   )
 
-  if ( a.tfin == null ) {
-    if ( b.tfin == null ) {
-      return a.tdue - b.tdue
-    }
-    return -1
-  } else if ( b.tfin == null ) {
-    return 1
+// Given time t and a promise p, return the absolute distance in seconds 
+// between t and the promise's Schelling fence nearest to t.
+// If the deadline is null then return tini minus t, so lack of deadline sorts
+// to the top and, among those, ties are broken so that promises created 
+// earlier appear first. (And if promises with no deadlines have creation dates
+// in the future for some reason, those may in fact sort later. That's probably
+// (a) reasonable and (b) almost always moot.)
+function scheldist(t, p) {
+  const tini = p.tini.getTime()/1000
+  if (p.tdue === null) { return tini - t }
+  const tdue = p.tdue.getTime()/1000
+  return Math.min(
+    Math.abs(t - tdue),         // abs difference between t  &  deadline
+    Math.abs(t - tdue - 60),    //                              1 minute late
+    Math.abs(t - tdue - 3600),  //                              1 hour late
+    Math.abs(t - tdue - SID),   //                              1 day late
+    Math.abs(t - tdue - SIW),   //                              1 week late
+    Math.abs(t - tdue - SIM),   //                              1 month late
+    Math.abs(t - tdue - SIY),   // abs difference between t  &  1 year late
+  )
+}
+
+export const promiseGallerySort = (a, b) => {
+  const a_tfin = a.tfin === null ? null : a.tfin.getTime()/1000
+  const b_tfin = b.tfin === null ? null : b.tfin.getTime()/1000
+  if (a_tfin === null && b_tfin === null) {  // sort by decreasing urgency
+    const now = Date.now()/1000
+    const a_tdue = a.tdue === null ? null : a.tdue.getTime()/1000
+    const b_tdue = b.tdue === null ? null : b.tdue.getTime()/1000
+    return sheldist(now, a_tdue) - sheldist(now, b_tdue)
+    // Or the nice simple thing to do would be to always sort in plain old
+    // due date order like so:
+    // return a_tdue - b_tdue
   }
-  return b.tfin - a.tfin
+  if (a_tfin === null) {  // only promise a is incomplete so sort it first
+    return -1 
+  }
+  if (b_tfin === null) {  // only promise b is incomplete so sort it first
+    return 1 
+  }
+  return b_tfin - a_tfin  // sort by decreasing completion date
 }

27686679897789b527622864ae1beccf82582350
Chris Butler <chris@platformthirteen.com>
2019-01-22 19:15:04 -0500

    add google analytics to react

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import * as ReactGA from 'react-ga';
 
 import App from 'src/app';
 
@@ -7,3 +8,6 @@ const root = document.createElement('div');
 document.body.appendChild(root);
 
 render(<App />, root);
+
+ReactGA.initialize('UA-121792102-1');
+ReactGA.pageview(window.location.pathname + window.location.search);

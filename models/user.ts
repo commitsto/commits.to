6d1b68d73560bd2f9aa65a1de7ca55c68fd07b68
Chris Butler <chris@platformthirteen.com>
2019-09-21 12:48:45 -0400

    move user pledge fetching to User model, clean up api endpoint

--- models/user.ts
+++ models/user.ts
@@ -1,5 +1,8 @@
 import { Users } from 'models/db';
 
+import { calculateReliability } from 'lib/parse/credit';
+import promiseGallerySort from 'lib/sort';
+
 class User {
   public static _dbModel = Users; // tslint:disable-line variable-name
 
@@ -7,6 +10,31 @@ class User {
     model: User._dbModel,
     where: { username }
   })
+
+  public static pledges = ({ username }) =>
+    Users.findOne({
+      where: {
+        username,
+      }
+    }).then((user) => {
+      if (user) {
+        return user.getValidPromises().then((promises) => {
+          const { score, counted } = calculateReliability(promises);
+
+          user.update({ score, counted, pending: promises.length - counted });
+          promises.sort(promiseGallerySort);
+
+          return {
+            counted,
+            pending: promises.length - counted,
+            promises,
+            reliability: score,
+            user,
+          };
+        });
+      }
+      return false;
+    })
 
 }
 

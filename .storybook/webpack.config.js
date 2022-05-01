7e05cd80c62cededb3cbff045c59d3418f99e698
Chris Butler <chris@platformthirteen.com>
2022-05-01 13:38:50 -0400

    upgrade and fix storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,13 @@
 const webpackConfig = require('../.webpack/config');
 
-module.exports = (storybookBaseConfig) => {
-  storybookBaseConfig.resolve.alias = { ...webpackConfig.resolve.alias };
-  storybookBaseConfig.resolve.extensions.push('.ts', '.tsx');
-  storybookBaseConfig.module.rules[0].test = /\.(jsx?|tsx?)$/;
+module.exports = {
+  webpackFinal: async (config) => {
 
-  return storybookBaseConfig;
+    config.resolve = {
+      ...config.resolve,
+      alias: { ...webpackConfig.resolve.alias },
+    };
+  
+    return config;
+  },
 };
